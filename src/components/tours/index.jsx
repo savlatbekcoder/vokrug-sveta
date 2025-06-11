2f4c66880f7367eda6e0b6909ff099abef761eae
savlatbekcoder <savlatbek009@gmail.com>
2025-06-11 21:55:48 +0500

    code!

--- src/components/tours/index.jsx
+++ src/components/tours/index.jsx
@@ -4,15 +4,12 @@ import { useNavigate } from "react-router-dom";
 import "./style.css";
 import { IoOpenOutline } from "react-icons/io5";
 import { tours } from "../../tour_data";
-import GTranslateWidget from "../lang";
 
 const Tours = () => {
   const navigate = useNavigate();
 
   return (
     <section id="tours">
-      {/* <GTranslateWidget /> */}
-
       <div className="cotainer">
         <h1 className="main_text">Tours</h1>
         <div className="tours_row">
